1e0f6231c48e81d1807b8a24770723b0bacc16eb
drillbits <neji@drillbits.jp>
2019-01-18 14:11:02 +0900

    add Go and dotfiles

--- init_user.sh
+++ init_user.sh
@@ -19,3 +19,26 @@ echo '>>> Setup fonts'
 set -x
 pikaur -S --noconfirm noto-fonts-cjk-otf adobe-source-han-serif-otc-fonts
 { set +x; } 2>/dev/null
+
+echo '>>> Setup Go'
+set -x
+wget https://dl.google.com/go/go1.4.3.linux-amd64.tar.gz
+tar xvf go1.4.3.linux-amd64.tar.gz
+mv go go1.4
+rm go1.4.3.linux-amd64.tar.gz
+mkdir -p ~/src/github.com/golang
+cd ~/src/github.com/golang
+git clone https://github.com/golang/go.git
+cd go
+git checkout go1.11.4
+cd src
+./make.bash
+{ set +x; } 2>/dev/null
+
+echo '>>> Setup dotfiles'
+mkdir -p ~/src/github.com/drillbits
+cd ~/src/github.com/drillbits
+git clone git@github.com:drillbits/dotfiles.git
+cd dotfiles
+make
+{ set +x; } 2>/dev/null
